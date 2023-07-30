5e92c85556e31471b346c82c77c62c2e11137bbd
Alex Miserandino <amiserandino20@gmail.com>
2023-07-30 14:32:35 -0700

    added mongoose and MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +18,10 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl', (req, res) => {
+
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
